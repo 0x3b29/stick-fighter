e7331d2bcb61af2d217961667654e536717a7c34
oli4@home <wagener_olivier@hotmail.com>
2019-10-01 23:01:50 +0200

    add keyboard support

--- Assets/Scripts/StickFighter.cs
+++ Assets/Scripts/StickFighter.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 public class StickFighter : MonoBehaviour
 {
     public int player;
+
     private float disableParticleSystem;
     private int health = 100;
+    private const float legSlurp = 0.075f;
+    private bool resetLeftLegWithoutSlerp = false;
+    private bool resetRightLegWithoutSlerp = false;
+
     public RectTransform foregroundPanel;
-    GameObject stickFighter;
-    Transform leg1Parent;
-    Transform leg2Parent;
-    ParticleSystem.EmissionModule emissionModule;
+
+    // Stick fighter
+    private GameObject stickFighter;
+    private Transform leg1Parent;
+    private Transform leg2Parent;
+    private ParticleSystem.EmissionModule emissionModule;
+
+    // Keyboard inputs
+    public KeyCode rotateLeft;
+    public KeyCode rotateRight;
+    public KeyCode jump;
+    public KeyCode hit;
+    public KeyCode flip;
+    public KeyCode leftLeg;
+    public KeyCode rightLeg;
 
     // Start is called before the first frame update
     void Start()
@@ -26,26 +42,104 @@ public class StickFighter : MonoBehaviour
     void Update()
     {
         // Adjust the angle of the legs (Does not really adds to the game but whatever)
-        leg1Parent.rotation = Quaternion.Euler(new Vector3(0, 0, stickFighter.transform.rotation.eulerAngles.z + ((Input.GetAxis("Joy" + player + "Axis1") - 0.5f) * 100)));
-        leg2Parent.rotation = Quaternion.Euler(new Vector3(0, 0, stickFighter.transform.rotation.eulerAngles.z - ((Input.GetAxis("Joy" + player + "Axis2") - 0.5f) * 100)));
 
-        // Rotate the entire prefab using the left analogue stick
+        // Set the left leg
+        float leftLegControllerInput = Input.GetAxis("Joy" + player + "Axis1");
+        if (leftLegControllerInput != 0 || resetLeftLegWithoutSlerp)
+        {
+            // The controller is beeing used
+            leg1Parent.rotation = Quaternion.Euler(new Vector3(0, 0, stickFighter.transform.rotation.eulerAngles.z + (leftLegControllerInput - 0.5f) * 100));
+            
+            // If controller axis was beeing used, this flag must be set to avoid slerp. 
+            if (leftLegControllerInput != 0)
+            {
+                resetLeftLegWithoutSlerp = true;
+            }  
+            else
+            {
+                resetLeftLegWithoutSlerp = false;
+            }
+        }
+        else
+        {
+            // The controller is not beeing used (or in neutral)
+            if (Input.GetKey(leftLeg))
+            {
+                Quaternion stop = Quaternion.Euler(new Vector3(0, 0, stickFighter.transform.rotation.eulerAngles.z + (1f - 0.5f) * 100));
+                leg1Parent.rotation = Quaternion.Slerp(leg1Parent.rotation, stop, legSlurp);
+            }
+            else
+            {
+                Quaternion stop = Quaternion.Euler(new Vector3(0, 0, stickFighter.transform.rotation.eulerAngles.z + (0 - 0.5f) * 100));
+                leg1Parent.rotation = Quaternion.Slerp(leg1Parent.rotation, stop, legSlurp);
+            }
+        }
+
+        // Set the right leg
+        float rightLegControllerInput = Input.GetAxis("Joy" + player + "Axis2");
+        if (rightLegControllerInput != 0 || resetRightLegWithoutSlerp)
+        {
+            // The controller is beeing used
+            leg2Parent.rotation = Quaternion.Euler(new Vector3(0, 0, stickFighter.transform.rotation.eulerAngles.z - (rightLegControllerInput - 0.5f) * 100));
+
+            // If controller axis was beeing used, this flag must be set to avoid slerp. 
+            if (rightLegControllerInput != 0)
+            {
+                resetRightLegWithoutSlerp = true;
+            }
+            else
+            {
+                resetRightLegWithoutSlerp = false;
+            }
+        }
+        else
+        {
+            // The controller is not beeing used (or in neutral)
+            if (Input.GetKey(rightLeg))
+            {
+                Quaternion stop = Quaternion.Euler(new Vector3(0, 0, stickFighter.transform.rotation.eulerAngles.z - (1f - 0.5f) * 100));
+                leg2Parent.rotation = Quaternion.Slerp(leg2Parent.rotation, stop, legSlurp);
+            }
+            else
+            {
+                Quaternion stop = Quaternion.Euler(new Vector3(0, 0, stickFighter.transform.rotation.eulerAngles.z - (0 - 0.5f) * 100));
+                leg2Parent.rotation = Quaternion.Slerp(leg2Parent.rotation, stop, legSlurp);
+            }
+        }
+
+        // Rotate the fighter
+
+        // Using the left analogue stick
         stickFighter.transform.Rotate(0, 0, (Input.GetAxis("Joy" + player + "Axis3") * 1.7f));
 
-        // Swing the sword using the xbox one controller's A button
-        if (Input.GetKeyDown("joystick " + player + " button 0"))
+        // Using the rotate keys
+        if (Input.GetKey(rotateLeft))
+        {
+            stickFighter.transform.Rotate(0, 0, (-1.7f));
+        }
+
+        if (Input.GetKey(rotateRight))
+        {
+            stickFighter.transform.Rotate(0, 0, (1.7f));
+        }
+
+        // Swing the sword 
+        // using the xbox one controller's A button OR the keyboard's hit key  
+        if (Input.GetKeyDown("joystick " + player + " button 0") || Input.GetKeyDown(hit))
         {
             stickFighter.GetComponent<Animator>().Play("Hit");
         }
 
-        // Flip the stick figther along his X axis using the xbox one controller's B button
-        if (Input.GetKeyDown("joystick " + player + " button 1"))
+        // Flip the stick figther along his X axis 
+        // using the xbox one controller's B button OR the keyboard's flip key
+        if (Input.GetKeyDown("joystick " + player + " button 1") || Input.GetKeyDown(flip))
         {
             stickFighter.transform.localScale = new Vector3(stickFighter.transform.localScale.x * -1, stickFighter.transform.localScale.y, stickFighter.transform.localScale.z);
         }
 
         // Give the stickfigter a little jetpack like push to make flying moves possible
-        if (Input.GetKeyDown("joystick " + player + " button 2"))
+        // using the xbox one controller's X button OR the keyboard's jump key
+        if (Input.GetKeyDown("joystick " + player + " button 2") || Input.GetKeyDown(jump))
         {
             stickFighter.GetComponent<Rigidbody2D>().AddForce(stickFighter.transform.up * 150);
             
@@ -56,14 +150,16 @@ public class StickFighter : MonoBehaviour
             disableParticleSystem = Time.time + .3f;
         }
 
-        // Pressing the xbox one controller's menue button starts a new roung
-        if (Input.GetKeyDown("joystick " + player + " button 7"))
+        // Start a new roung
+        // pressing the xbox one controller's menue button OR the space bar
+        if (Input.GetKeyDown("joystick " + player + " button 7") || Input.GetKeyDown(KeyCode.Space))
         {
             Main.instance.Replay();
         }
 
-        // Pressing the xbox one controller's back button quits the game
-        if (Input.GetKeyDown("joystick " + player + " button 6"))
+        // Quits the game
+        // pressing the xbox one controller's back button OR the escape key
+        if (Input.GetKeyDown("joystick " + player + " button 6") || Input.GetKeyDown(KeyCode.Escape))
         {
             Main.instance.Quit();
         }
